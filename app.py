6e09527f6e933a68219a4d4e5a2e34bc23127fbc
Robrod <rrodas182@gmail.com>
2025-05-12 15:23:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-# Flask app principal
+from flask import Flask
+
+app = Flask(__name__)  # ← Esta línea es indispensable
+
+@app.route("/")
+def home():
+    return "¡La app de Madres está funcionando en Render!"
+
 if __name__ == "__main__":
     from os import environ
     app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
